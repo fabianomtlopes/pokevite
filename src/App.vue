a6e2af13983672ff66cdb9306649252a9180801a
Fabiano de Matos Lopes <fabianomtlopes@yahoo.com.br>
2023-04-24 11:13:43 -0300

    Deploy do App PokeVite

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<script setup>
+    import {ref} from 'vue'
+
+    let num = ref(0)
+</script>
+
+<template>
+        <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="container-fluid">
+            <RouterLink class="navbar-brand" to="/">
+                <img src="https://cdn-icons-png.flaticon.com/512/188/188987.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+                PokeVite
+            </RouterLink>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                    <RouterLink class="nav-link" aria-current="page" to="/">Home</RouterLink>
+                    </li>
+                    <li class="nav-item">
+                    <router-link class="nav-link" to="/about">About</router-link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <!-- CONTENT  -->
+    <div class="main">
+        <RouterView></RouterView>
+    </div>
+
+    <footer class="bg-primary text-light">
+        <div>Design in {{ new Date().getFullYear() }}</div>
+    </footer>
+</template>
+
+<style>
+    body {
+        height: 100vh;
+        background: rgb(106,196,233);
+        background: linear-gradient(0deg, rgba(179,219,242,1) 0%, rgba(4,63,163,1) 100%);
+    }
+    footer {
+        position: fixed;
+        bottom: 0;
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 56px;
+    }
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container mt-4">
+    <div class="card">
+        <h1 class="text-center pt-4">Para  meu afilhado Eduardo</h1>
+        <hr>
+        <div class="card-body text-center">
+            Aqui está uma coleção do Pokemon para você. Apenas para se divertir um pouco.
+        </div>
+    </div>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
